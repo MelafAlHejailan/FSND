9194fe24ca6836d51a3a995ed0761dffe5ffdc36
melaf <melaf103@gmail.com>
2020-12-23 20:37:11 +0300

    your comment

--- projects/01_fyyur/starter_code/models.py
+++ projects/01_fyyur/starter_code/models.py
@@ -1,11 +1,12 @@
 # Import db
 
-from app import db
-
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 # Venue Model
+
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -22,13 +23,15 @@ class Venue(db.Model):
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
-# On delete we need to use cascade=" all, delete-orphan" because of the dependence between venue and show.  
-    show = db.relationship('Show', backref='venue', lazy=True, cascade="all, delete-orphan")
+# On delete we need to use cascade=" all, delete-orphan" because of the dependence between venue and show.
+    show = db.relationship('Show', backref='venue',
+                           lazy=True, cascade="all, delete-orphan")
 
     def __repr__(self):
         return f'<Venue ID: {self.id}, name: {self.name}'
 
 # Artist Model
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -50,13 +53,17 @@ class Artist(db.Model):
         return f'<Artist ID: {self.id}, name: {self.name}>'
 
 # Show Model
+
+
 class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-# On delete we need to use ondelete="CASCADE" because of the dependence between venue and show.  
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id', ondelete="CASCADE"), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+# On delete we need to use ondelete="CASCADE" because of the dependence between venue and show.
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'Venue.id', ondelete="CASCADE"), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'Artist.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
